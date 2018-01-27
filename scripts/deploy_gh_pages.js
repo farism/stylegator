6958abe25161143b5d0fba6537d5798f967de1b2
Faris Mustafa <farismmk@gmail.com>
2018-01-27 15:33:08 -0800

    do full gh pages

--- scripts/deploy_gh_pages.js
+++ scripts/deploy_gh_pages.js
@@ -7,8 +7,6 @@ const cwd = path.join(process.cwd(), 'tmp/gh-pages')
 
 ;(async () => {
   const target = process.argv.slice(2)[0]
-  console.log(target)
-  return
   const remote = await exec('git config remote.origin.url')
   await exec(`mkdirp ${cwd}`)
   await exec(
