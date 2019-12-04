2561e9b6756b4abf3498adbeab665a28606186b9
Faris Mustafa <farismmk@gmail.com>
2019-12-03 17:56:30 -0800

    flatten versions

--- v/0.19.0/6.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"./src/sections/guides/static.md":function(n,t){n.exports="# Static Content\n\nStylegator supports static markdown by simply using\n\n# Examples\n\n## html\n\n```md\n ```html\n <button>label</button>\n ```\n```\n\nresults in:\n\n```html\n<button>label</button>\n```\n\n## images\n\n```md\n![alt text](assets/logo.png)\n```\n\nresults in:\n\n![alt text](assets/logo.png)\n"}}]);
-//# sourceMappingURL=6.js.map
